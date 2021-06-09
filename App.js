1cd8c189871ea6b4b784c553d281e32282f0b4dd
Elad Cohen <eladc.android@gmail.com>
2021-06-09 16:28:08 +0300

    Use card to show location

--- App.js
+++ App.js
@@ -1,21 +1,79 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect, useCallback } from "react";
+import { Platform, Text, View, StyleSheet, SafeAreaView } from "react-native";
+import * as Location from "expo-location";
+import {
+  Avatar,
+  Button,
+  Card,
+  Title,
+  Paragraph,
+  FAB,
+} from "react-native-paper";
 
 export default function App() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  const getLocation = useCallback(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = "Waiting..";
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <>
+        <Card style={{ margin: 10 }}>
+          <Card.Title
+            title="Location App"
+            subtitle="React native - expo example"
+          />
+          <Card.Content>
+            <Title>Latitude</Title>
+            <Paragraph>{location?.coords?.latitude}</Paragraph>
+            <Title>Longitude</Title>
+            <Paragraph>{location?.coords?.longitude}</Paragraph>
+          </Card.Content>
+          <Card.Actions></Card.Actions>
+        </Card>
+        <FAB
+          style={styles.fab}
+          small
+          icon="map"
+          onPress={() => getLocation()}
+        />
+      </>
+      {/* <Text style={styles.paragraph}>{text}</Text> */}
+      {/* </View> */}
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  fab: {
+    position: "absolute",
+    margin: 16,
+    right: 0,
+    bottom: 0,
   },
 });
